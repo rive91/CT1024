11bde28a7b415fc338dc12dc442207f69b3747b8
rive <rive@MSI>
2024-10-05 11:29:30 -0600

    Primer Commit

---
+++ 1024/src/test/java/pruebaTestNG/Selenium_Test.java
@@ -0,0 +1,13 @@
+package pruebaTestNG;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Selenium_Test {
+  @Test
+  public void f() {
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://www.google.com");
+  }
+}
